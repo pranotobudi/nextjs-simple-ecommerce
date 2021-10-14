5f5f4f2fead60dd9c21af3898ada84e39be7ce41
Pranoto Budi <pranotobudi.s@gmail.com>
2021-10-14 17:36:07 +0700

    part 04, Product component, ProductContainer component

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,11 @@
 import Head from "next/head";
 import Header from "../components/Header";
-
-export default function Home() {
+import ProductContainer from "../components/ProductContainer";
+import {myDummyProducts} from "/public/dummyData"
+export default function Home({products}) {
+  // console.log(products)
   return (
-    <div>
+    <div className="bg-blue-100">
       <Head>
         <link rel="shortcut icon" href="/favicon.png" />
         <title>Gophers Art Shop</title>
@@ -11,8 +13,21 @@ export default function Home() {
         <Header />
       <main className="max-w-screen-2xl mx-auto">
         {/* Product List */}
-        Product List..
-        </main>
+        <ProductContainer productLists={products} />
+      </main>
     </div>
   )
+}
+
+
+export async function getServerSideProps(context){
+  // const products = await fetch('https://fakestoreapi.com/products')
+  // .then(res=>res.json()); 
+  const products = myDummyProducts
+  // console.log(products)
+  return {
+    props: {
+      products,
+    },
+  };
 }
---
+++ public/dummyData.js
@@ -0,0 +1,100 @@
+const myDummyProducts = [
+    {
+        id:1,
+        title:'Art 01',
+        price:'7',
+        category:'category 01',
+        description:'Description for Art 01',
+        image:'/products/product01.jpg'
+    },
+    {
+        id:2,
+        title:'Art 02',
+        price:'11',
+        category:'category 01',
+        description:'Description for Art 01',
+        image:'/products/product02.jpg'
+    },
+    {
+        id:3,
+        title:'Art 03',
+        price:'9',
+        category:'category 01',
+        description:'Description for Art 01',
+        image:'/products/product03.jpg'
+    },
+    {
+        id:4,
+        title:'Art 04',
+        price:'13',
+        category:'category 01',
+        description:'Description for Art 01',
+        image:'/products/product04.jpg'
+    },
+    {
+        id:5,
+        title:'Art 05',
+        price:'15',
+        category:'category 01',
+        description:'Description for Art 01',
+        image:'/products/product05.jpg'
+    },
+    {
+        id:6,
+        title:'Art 06',
+        price:'11',
+        category:'category 01',
+        description:'Description for Art 01',
+        image:'/products/product06.jpg'
+    },
+    {
+        id:7,
+        title:'Art 07',
+        price:'7',
+        category:'category 01',
+        description:'Description for Art 01',
+        image:'/products/product07.jpg'
+    },
+    {
+        id:8,
+        title:'Art 08',
+        price:'11',
+        category:'category 01',
+        description:'Description for Art 01',
+        image:'/products/product08.jpg'
+    },
+    {
+        id:9,
+        title:'Art 09',
+        price:'9',
+        category:'category 01',
+        description:'Description for Art 01',
+        image:'/products/product09.jpg'
+    },
+    {
+        id:10,
+        title:'Art 10',
+        price:'7',
+        category:'category 01',
+        description:'Description for Art 01',
+        image:'/products/product10.jpg'
+    },
+    {
+        id:11,
+        title:'Art 11',
+        price:'9',
+        category:'category 01',
+        description:'Description for Art 01',
+        image:'/products/product11.jpg'
+    },
+    {
+        id:12,
+        title:'Art 12',
+        price:'15',
+        category:'category 01',
+        description:'Description for Art 01',
+        image:'/products/product12.jpg'
+    },
+]
+
+export {myDummyProducts}
