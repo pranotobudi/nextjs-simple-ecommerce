c7ec603d06c08ec5306dc2d014675cc29f870a57
Pranoto Budi <pranotobudi.s@gmail.com>
2021-10-17 16:17:06 +0700

    clean up code part 04

--- components/Product.js
+++ components/Product.js
@@ -1,34 +1,10 @@
 import { StarIcon } from "@heroicons/react/solid";
-// import {useState} from "react"
 import Image from "next/image";
-// import Currency from "react-currency-formatter";
-// import { useDispatch } from "react-redux";
-// import { addToBasket } from "../slices/basketSlice";
 
 
-// const MAX_RATING = 5;
-// const MIN_RATING = 1;
 function Product({id, title, price, description, category, image, rating}){
-    // const dispatch = useDispatch();
-
-    // const [rating] = useState(
-    //     Math.floor(Math.random() * (MAX_RATING - MIN_RATING + 1) + MIN_RATING)
-    // );
-    // const [hasPrime] = useState(
-    //     Math.random() < 0.5
-    // )
-
-    // const addItemToBasket = () => {
-    //     const product = {
-    //         id, title, price, rating, description, category, image, hasPrime,
-    //     };
-     
-    //     dispatch(addToBasket(product))
-    // }
-
     return (
         <div className="relative flex flex-col m-5 bg-white z-30 p-10">
-            {/* <p className="absolute top-2 right-2 text-xs italic text-gray-400">{category}</p> */}
             <Image src={image} height={200} width={200} objectFit="contain" />
             <h4 className="my-3 self-center">{title}</h4>
             <div className="flex self-center">
@@ -38,10 +14,8 @@ function Product({id, title, price, description, category, image, rating}){
             </div>
             <p className="text-xs my-2 line-clamp-2 self-center">{description}</p>
             <div className="inline-block self-center">
-                {/* <Currency quantity={price} currency="USD" /> */}
                 {`$` + price}
             </div>
-            {/* <button onClick={addItemToBasket} className="mt-auto button">Add to Basket</button> */}
             <button className="mt-auto button">Add to Basket</button>
 
         </div>
--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,6 @@ import Header from "../components/Header";
 import ProductContainer from "../components/ProductContainer";
 import {myDummyProducts} from "/public/dummyData"
 export default function Home({products}) {
-  // console.log(products)
   return (
     <div className="bg-blue-100">
       <Head>
@@ -21,10 +20,7 @@ export default function Home({products}) {
 
 
 export async function getServerSideProps(context){
-  // const products = await fetch('https://fakestoreapi.com/products')
-  // .then(res=>res.json()); 
   const products = myDummyProducts
-  // console.log(products)
   return {
     props: {
       products,
